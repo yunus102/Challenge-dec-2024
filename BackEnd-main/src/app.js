d546143c6a3173a83a709102777e891dda4f59e0
yunus102 <itsyunus102@gmail.com>
2024-12-17 15:34:19 +0530

    fix cors error again 2

--- BackEnd-main/src/app.js
+++ BackEnd-main/src/app.js
@@ -12,7 +12,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 app.use(cors({
-    origin: "challenge-dec-2024.onrender.com",
+    origin: "https://challenge-dec-2024.onrender.com",
     credentials: true
 }))
 
